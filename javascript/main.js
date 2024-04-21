b81a31f5b9d89906400dc0c0e4f28f141fbabf54
Jeevan Miranda <jeevan_miranda@hotmail.com>
2024-04-22 09:44:53 +1000

    json file added. test data included

---
+++ javascript/main.js
@@ -0,0 +1,3 @@
+//create objects for each of the error codes with the description and solution as elements within the object
+//move objects into an array
+//create a functon to search the array possibly using find or findIndex methods
